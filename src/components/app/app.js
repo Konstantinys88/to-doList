905c09046da820ae2dd8a5f51eb19767e425aace
Konstantin0 <maschkovc@yandex.ru>
2023-05-26 17:48:45 +0300

    increase

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,10 +9,10 @@ import "./app.css";
 function App() {
 
     const data = [
-        { todo: "Проснутся", todoStatus: 8, id: 1 },
-        { todo: "Покормить кота", todoStatus: 10, id: 2 },
-        { todo: "Погладить кота", todoStatus: 9, id: 3 },
-        { todo: "Погладить кота", todoStatus: 9, id: 3 },
+        { todo: "Проснутся", todoStatus: 8, id: 1, increase: false },
+        { todo: "Покормить кота", todoStatus: 10, id: 2, increase: true },
+        { todo: "Погладить кота", todoStatus: 9, id: 3, increase: false },
+        { todo: "Убрать лоток кота", todoStatus: 9, id: 4, increase: false },
     ];
 
     return (
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,8 +1,17 @@
 import "./todo-list-item.css";
 
-const TodoListItem = ({todo, todoStatus}) => {
+
+
+const TodoListItem = ({ todo, todoStatus, increase }) => {
+    let classNameLi = "list-group-item d-flex justify-content-between";
+    if (increase) {
+        classNameLi += " increase";
+    }
+
+
+
     return (
-        <li className="list-group-item d-flex justify-content-between">
+        <li className={classNameLi}>
             <span className="list-group-item-label">{todo}</span>
             <input type="text" className="list-group-item-input" defaultValue={todoStatus} />
             <div className='d-flex justify-content-center align-items-center'>
